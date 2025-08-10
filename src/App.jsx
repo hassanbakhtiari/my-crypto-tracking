a803fd0051f76884840cdcf6c1e327bccd9d929b
hassan bakhtiari <bakhtiarihassan19656@gmail.com>
2025-08-10 11:29:52 -0700

    impliment base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
+import FooterComponent from "./components/ui/Footer.component"
+import NavbarComponent from "./components/ui/Navbar.component" 
 
 function App() {
 
   return (
-    <>
-     Hello World
-    </>
+    <div>
+      {/* Navbar */}
+        <NavbarComponent />
+         <div className="wrapper">
+           <h2>
+              Wrapper
+           </h2>
+          </div>
+        <FooterComponent />
+      {/* Wrapper */}
+
+      {/* Footer */}
+    </div>
+     
+    
   )
 }
 
