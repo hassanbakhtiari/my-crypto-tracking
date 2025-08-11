493b1710d355f88a14c55491e8e62a477295e687
hassan bakhtiari <bakhtiarihassan19656@gmail.com>
2025-08-11 03:38:43 -0700

    route implimented

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import FooterComponent from "./components/ui/Footer.component"
-import NavbarComponent from "./components/ui/Navbar.component" 
+/** @format */
+
+import FooterComponent from './components/ui/Footer.component';
+import NavbarComponent from './components/ui/Navbar.component';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/Home.page';
+import FAQPage from './pages/FAQ.page';
+import AboutUsPage from './pages/AboutUs.page';
+import ContactUsPage from './pages/ContactUs.page';
 
 function App() {
+	return (
+		<div>
+			{/* Navbar */}
+			<NavbarComponent />
 
-  return (
-    <div>
-      {/* Navbar */}
-        <NavbarComponent />
-         <div className="wrapper">
-           <h2>
-              Wrapper
-           </h2>
-          </div>
-        <FooterComponent />
-      {/* Wrapper */}
+			{/* Wrapper */}
+			<div className="wrapper">
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/faq" element={<FAQPage />} />
+					<Route path="/aboutus" element={<AboutUsPage />} />
+					<Route path="/contactus" element={<ContactUsPage />} />
+				</Routes>
+			</div>
 
-      {/* Footer */}
-    </div>
-     
-    
-  )
+			<FooterComponent />
+
+			{/* Footer */}
+		</div>
+	);
 }
 
-export default App
+export default App;
