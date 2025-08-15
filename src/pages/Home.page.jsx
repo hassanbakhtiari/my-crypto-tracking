022cc54116a4d17438293e75cb73f3b9ead4c78d
hassan bakhtiari <bakhtiarihassan19656@gmail.com>
2025-08-15 09:57:48 -0700

    end of project

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,12 +1,53 @@
 /** @format */
 
-import React from 'react';
+/** @format */
+
+import React, { useEffect, useState } from 'react';
 import Spinner from '../components/ui/Spinner.component.jsx';
+import axios from 'axios';
+import { baseUrl } from '../constants/api.js';
+import CoinsComponent from '../components/coins/Coins.component.jsx';
 
 const HomePage = () => {
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState('');
+
+	useEffect(() => {
+		setLoading(true);
+		axios(baseUrl)
+			.then((response) => {
+				setLoading(false);
+				console.log(response.data);
+				setData(response.data);
+			})
+			.catch((error) => {
+				setError('an error occurs');
+			});
+	}, []);
+
 	return (
-		<div>
-			<h1>HomePage</h1>
+		<div className="container">
+			<div className="row">
+				<div className="col-12 text-center">
+					<h1>Crypto Tracking</h1>
+					<h4>A place to manage of your trade information and action</h4>
+					<h4>Do'nt forget to alwayse asset and risk managment!!!!!!!!!</h4>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-12">
+					{loading ? (
+						<Spinner />
+					) : error !== '' ? (
+						<h1>{error}</h1>
+					) : (
+						<CoinsComponent data={data} />
+					)}
+				</div>
+			</div>
+
+			{/* <Spinner /> */}
 		</div>
 	);
 };
